c616fcfc4e2c31be1abf816e12deb45736315422
lk <lkghost7@gmail.com>
2018-10-27 23:52:00 +0300

    sql + 2 класса

--- database/src/main/java/by/itacademy/model/SystemUser.java
+++ database/src/main/java/by/itacademy/model/SystemUser.java
@@ -1,58 +1,44 @@
 package by.itacademy.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "system_users")
 @NoArgsConstructor
 @ToString
 public class SystemUser extends BaseEntity {
-    @Getter
-    @Setter
+
     @Column(name = "name")
     private String nameUser;
 
-    @Getter
-    @Setter
     @Column(name = "family")
     private String familyUser;
 
     @NotEmpty
-    @Getter
-    @Setter
     @Column(name = "e_mail", nullable=false)
     private String email;
 
     @NotEmpty
-    @Getter
-    @Setter
     @Column(name = "password", nullable=false)
     private String passwordUser;
 
-    @Getter
-    @Setter
-    @JoinTable(name = "users_privileges",
+     @JoinTable(name = "users_privileges",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "privilege_id"))
     @ManyToMany(fetch = FetchType.EAGER)
     private Set<Privilege> privilege = new HashSet<>();
 
-    @Getter
-    @Setter
     @Column(name = "gender")
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @Getter
-    @Setter
     @Embedded
     @AttributeOverrides({
             @AttributeOverride(name = "country", column = @Column(name = "home_region_country")),
@@ -60,8 +46,6 @@ public class SystemUser extends BaseEntity {
     })
     private Address firstAddress;
 
-    @Getter
-    @Setter
     @Embedded
     @AttributeOverrides({
             @AttributeOverride(name = "country", column = @Column(name = "payment_country")),
