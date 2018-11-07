2fe9592e40bf7a3cc3b85e9c47d05a4feaf5fa93
lk <lkghost7@gmail.com>
2018-11-07 22:48:36 +0300

    правка

--- database/src/test/java/by/itacademy/dao/CompanDaoImplTest.java
+++ database/src/test/java/by/itacademy/dao/CompanDaoImplTest.java
@@ -4,6 +4,9 @@ import by.itacademy.dao.generic.BaseDao;
 import by.itacademy.dao.generic.BaseDaoTest;
 import by.itacademy.dao.generic.CompanDaoImpl;
 import by.itacademy.model.Company;
+import org.junit.Test;
+
+import java.util.List;
 
 public class CompanDaoImplTest extends BaseDaoTest<Company> {
 
@@ -19,4 +22,17 @@ public class CompanDaoImplTest extends BaseDaoTest<Company> {
         return new Company();
     }
 
+//    @Test
+//    public void save() {
+//        Company company = new Company();
+//        company.setName("Vinty-Company");
+//       DAO.save(company);
+//    }
+//
+//    @Test
+//    public void findAll() {
+//        List<Company> all = DAO.findAll();
+//        System.out.println(all);
+//    }
+//
 }
