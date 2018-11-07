c801d346b0cc3f734a3c1acc37eac8c1ad939667
lk <lkghost7@gmail.com>
2018-11-07 22:15:35 +0300

    правки мастер №1

--- database/src/main/java/by/itacademy/dao/generic/BaseDao.java
+++ database/src/main/java/by/itacademy/dao/generic/BaseDao.java
@@ -2,9 +2,8 @@ package by.itacademy.dao.generic;
 
 import by.itacademy.model.BaseEntity;
 
-public interface BaseDao <T extends BaseEntity> {
+public interface BaseDao<T extends BaseEntity> {
 
     void save(T entity);
-
 
 }
