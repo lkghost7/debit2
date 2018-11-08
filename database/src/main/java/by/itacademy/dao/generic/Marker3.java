418acead1e7bdd9275b3fbed86fca3f7d89f1728
lk <lkghost7@gmail.com>
2018-11-08 22:33:26 +0300

    форсе пуш всей ветки 6

---
+++ database/src/main/java/by/itacademy/dao/generic/Marker3.java
@@ -0,0 +1,18 @@
+package by.itacademy.dao.generic;
+
+public class Marker3 {
+    public static void main(String[] args) {
+        System.out.println("marker5");
+        System.out.println("marker6");
+        System.out.println("marker6");
+        System.out.println("marker6");
+        System.out.println("marker6");
+        System.out.println("marker6");
+        System.out.println("marker6");
+        System.out.println("marker7");
+        System.out.println("marker7");
+        System.out.println("marker7");
+        System.out.println("marker7");
+    }
+
+}
