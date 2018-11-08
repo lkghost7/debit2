d24bc6dc8bfbd635223714fb9a3caf66af58815e
lk <lkghost7@gmail.com>
2018-11-08 22:17:17 +0300

    маркер №14

--- database/src/main/java/by/itacademy/dao/generic/Marker3.java
+++ database/src/main/java/by/itacademy/dao/generic/Marker3.java
@@ -3,6 +3,9 @@ package by.itacademy.dao.generic;
 public class Marker3 {
     public static void main(String[] args) {
         System.out.println("marker5");
+        System.out.println("marker6");
+        System.out.println("marker6");
+        System.out.println("marker6");
     }
 
 }
